9071ef44746a77bca055a991840163f4c5443982
Luka Selak <selakluka@gmail.com>
2023-06-23 12:20:48 +0200

    Initial Commit

---
+++ SlickUI/SlickUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlickUIApp.swift
+//  SlickUI
+//
+//  Created by Luka Selak on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SlickUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
